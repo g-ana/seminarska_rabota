563eceaed3515a08bf140f6a2ff55e67e68b8e68
Ana <40370137+g-ana@users.noreply.github.com>
2020-09-03 15:33:13 +0200

    Update CategoriesController.java

--- src/wp/src/main/java/com/example/wp/backend/web/rest_controllers/CategoriesController.java
+++ src/wp/src/main/java/com/example/wp/backend/web/rest_controllers/CategoriesController.java
@@ -18,7 +18,7 @@ public class CategoriesController {
         this.categoriesService = categoriesService;
     }
 
-    @GetMapping
+    @GetMapping("/all")
     public List<Map<String, String>> findAllCategories() {
         return this.categoriesService.findAllCategories();
     }
@@ -26,5 +26,12 @@ public class CategoriesController {
     @GetMapping("/{name}")
     public Map<String, String> findCategoryByName(@PathVariable String name) {
         return this.categoriesService.findCategoryByName(name);
+    }
+    
+    @GetMapping
+    public List<Map<String, String>> findCategoriesPaged(
+            @RequestHeader(name="page", required = false, defaultValue = "0") int page,
+            @RequestHeader(name="page-size", required = false, defaultValue = "2") int pageSize) {
+        return this.categoriesService.findCategoriesPaged(page, pageSize);
     }
 }
