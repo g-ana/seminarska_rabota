ab7a08a983fe088beac6fd42663f089bf610915e
Ana <40370137+g-ana@users.noreply.github.com>
2020-09-15 16:31:32 +0200

    Add files via upload

---
+++ src/wp/src/main/java/com/example/wp/backend/service/AuthService.java
@@ -0,0 +1,10 @@
+package com.example.wp.backend.service;
+
+// import com.example.wp.backend.model.User;
+
+/* public interface AuthService {
+    public User getCurrentUser();
+    public Long getCurrentUserId();
+    public String getCurrentUserUsername();
+    public User signUpUser(String username, String password, String repeatedPassword);
+} */
---
+++ src/wp/src/main/java/com/example/wp/backend/service/UserDetailsService.java
@@ -0,0 +1,14 @@
+package com.example.wp.backend.service;
+
+/* import com.example.wp.backend.model.User;
+import com.example.wp.backend.model.exceptions.UserAlreadyExistsException;
+import com.example.wp.backend.model.exceptions.UserNotFoundException; */
+/* import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException; */
+
+/* public interface UserDetailsService extends UserDetails {
+
+//    public User findUserByUsername(String username) throws UsernameNotFoundException;
+    public User findUserById(Long id) throws UserNotFoundException;
+    public User registerUser(User user) throws UserAlreadyExistsException;
+} */
