089e800d54b3ee5260910dda604128c37fd125cc
Ana <40370137+g-ana@users.noreply.github.com>
2020-09-15 16:17:34 +0200

    Update WpApplication.java

--- src/wp/src/main/java/com/example/wp/WpApplication.java
+++ src/wp/src/main/java/com/example/wp/WpApplication.java
@@ -3,7 +3,10 @@ package com.example.wp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan("com.example.wbs")
@@ -11,6 +14,18 @@ public class WpApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WpApplication.class, args);
+    }
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET");
+            }
+        };
     }
 
 }
